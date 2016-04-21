9c577b583e0fd1c2fbdf0466575ceae6ca71b2c7
Bryan Thompson <bryan@mristek.com>
2016-04-21 17:18:29 -0400

    Understanding Collections Types in Swift Playground
    Code as presented in writeup.
    Added [BET] comments for additional testing.

---
+++ make_your_own_class_collections/make_your_own_class_collections.playground/Contents.swift
@@ -0,0 +1,128 @@
+// Understanding Collections Types in Swift — Part 3
+// https://medium.com/swift-programming/understanding-collections-types-in-swift-part-3-making-your-own-classes-collections-aabbf186b801#.k4hsfgsua
+
+import Cocoa
+
+struct Person: Hashable {
+    var firstName: String
+    var lastName: String
+    var email: String
+    
+    var hashValue: Int {
+        get {
+            return "first:\(firstName), last:\(lastName), email:\(email)".hashValue
+        }
+    }
+}
+
+// All objects that conform to Hashable require a global == implementation
+// removing this will cause an error to show up on Person
+func ==(lhs: Person, rhs: Person) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+}
+
+// Add custom printing
+// [BET] Why must it be by extension?
+extension Person: CustomStringConvertible {
+    var description: String {
+        get {
+            return "name: \(firstName) \(lastName) email: \(email)"
+        }
+    }
+}
+
+struct PeopleCollection: Hashable {
+    var people: Set<Person> = []
+    
+    mutating func add(person: Person) {
+        // [BET] - is the .contains call necessary?
+        if !people.contains(person) {
+            people.insert(person)
+        }
+    }
+    
+    mutating func remove(person: Person) -> Person? {
+        let removedPerson = people.remove(person)
+        return removedPerson
+    }
+    
+    func retrieveByFirstName(name: String) -> [Person]? {
+        let foundPeople = people.filter {
+            $0.firstName == name
+        }
+        return foundPeople
+    }
+    
+    func retrieveByLastName(name: String) -> [Person]? {
+        let foundPeople = people.filter {
+            $0.lastName == name
+        }
+        
+        return foundPeople
+    }
+    
+    func retrieveSharedContacts(contacts: PeopleCollection) -> Set<Person>? {
+        let commonContacts = self.people.intersect(contacts.people)
+        return commonContacts
+    }
+    
+    var count: Int {
+        get {
+            return people.count
+        }
+    }
+    
+    var hashValue: Int {
+        get {
+            return people.hashValue
+        }
+    }
+}
+
+func ==(lhs: PeopleCollection, rhs: PeopleCollection) -> Bool {
+    return lhs.hashValue == rhs.hashValue
+}
+
+extension PeopleCollection: CustomStringConvertible {
+    var description: String {
+        get {
+            var _description: String = ""
+            for person in self.people {
+                _description += "person: \(person) "
+            }
+            return _description
+        }
+    }
+}
+
+
+// [BET] Use the new collection
+let jack = Person(firstName: "Jack", lastName: "Jones", email: "jack_white@acme.com")
+let lee = Person(firstName: "Lee", lastName: "Black", email: "lb@acme.com")
+let roger = Person(firstName: "Roger", lastName: "Bland", email: "rb@xzy.com")
+let mary = Person(firstName: "Mary", lastName: "Jones", email: "mw@xyz.com")
+
+var peopleList = PeopleCollection()
+peopleList.add(jack)
+peopleList.add(lee)
+peopleList.add(roger)
+peopleList.add(mary)
+peopleList.count
+print(peopleList)
+
+let removeJack = peopleList.remove(jack)
+peopleList.add(removeJack!)
+
+let theJones = peopleList.retrieveByLastName("Jones")
+theJones?.count
+
+var contactList = PeopleCollection()
+contactList.add(Person(firstName: "Jack", lastName: "Jones", email: "jack_white@acme.com"))
+contactList.add(Person(firstName: "Lee", lastName: "Black", email: "lb@acme.com"))
+contactList.add(Person(firstName: "Robert", lastName: "Bland", email: "rb@xzy.com"))
+contactList.add(Person(firstName: "Mickey", lastName: "Jones", email: "mw@xyz.com"))
+
+let commonContacts = peopleList.retrieveSharedContacts(contactList)
+commonContacts?.count
+print(commonContacts!)
+
