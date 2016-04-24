d07902da19efef0e921eed4de1c5badd66363292
Bryan Thompson <bryan@mristek.com>
2016-04-23 22:25:29 -0400

    A Swift Refactor
    Code as presented before discussion and changes

---
+++ a_swift_refactor/a_swift_refactor.playground/Contents.swift
@@ -0,0 +1,33 @@
+// A Swift Refactor
+//  PunGame = .Strong
+// https://medium.com/the-traveled-ios-developers-guide/a-swift-refactor-831e9a8f18a#.d4mhvcdpg
+
+import Cocoa
+
+//Assume PlayerRound is a simple struct with a roundScore Int
+//CongratsMessage is also a struct with a roundCongrats String
+
+let aRound:PlayerRound? = PlayerRound()
+
+if aRound != nil
+{
+    if aRound?.roundScore? >= 5 && aRound?.roundScore? <= 10
+    {
+        print(“Score gets an A+”)
+        let iterationNum = [1,2,3]
+        for i in 0…iterationNum.count
+        {
+            print(“Awesome, we are showing a victory message!!”)
+            let message = self.randomCongrats()
+            switch message
+            {
+            case let message where message is String:
+                print(message)
+            case let message where message is CongratsMessage:
+                print(message.roundCongrats)
+            default:
+                print(“Uh oh”)
+            }
+        }
+    }
+}
